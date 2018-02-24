efe103cb12c0a88ddfebaebae69846a5ab9f3328
David Camara <jdpcii@gmail.com>
2018-02-23 23:03:22 -0500

    Working on reading and writing files

---
+++ challenges/diskio1Challenge.py
@@ -0,0 +1,10 @@
+"""
+ Read/Write Challenge
+"""
+
+__author__ = 'dev'
+with open('sample.txt', 'w') as tables:
+    for i in range (2, 13):
+        for j in range (1, 13):
+            print("{1:>2} times {0} is {2}".format(i, j, i * j), file=tables)
+        print("-" * 20, file=tables)
---
+++ diskio1.py
@@ -0,0 +1,42 @@
+"""
+ Read a File
+"""
+
+__author__ = 'dev'
+# fileop = open('/Users/mackyzacky/repos/python/base.py','r')
+# fileop = open("C:\\Documents and Settings\\mine\\My Documents\\base.py", 'r') # for windows
+# fileop = open('base.py', 'r')
+
+# for line in fileop:
+#     if 'author' in line.lower():
+#         print(line, end='')
+
+# fileop.close()
+
+# with open('join.py','r') as myfile:
+#     for line in myfile:
+#         if 'JOIN' in line.upper():
+#             print(line, end='')
+# # with catches errors and closes files automatically
+
+# with open('base.py','r') as myfile:
+#     line = myfile.readline()
+#     while line:
+#         print(line, end='')
+#         line = myfile.readline()
+
+# mylist = ["this hear", "cities", "rock and roll", "tonight"]
+
+# with open('outlist.txt', 'w') as list_file:
+#     for entry in mylist:
+#         print(entry, file=list_file) # do NOT put spaces in around the = sign
+
+# newkist = []
+# with open('outlist.txt', 'r') as newlist:
+#     for entry in newlist:
+#         newkist.append(entry.strip('\n'))
+
+# print(newkist)
+# for entry in newkist:
+#     print(entry)
+
